7b4b3a2340b42e4daa501cdf1c16e088d02ace60
S5198678 <100881826+S5198678@users.noreply.github.com>
2022-04-06 17:39:34 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, num_0=0, num_1=1, num_2;
+    cin >> n;
+    if (n==1) {
+        cout << "1" <<endl;
+    }
+    for (num_2=0; num_2<=n; num_2++) {
+        num_2=num_0+num_1;
+        if (num_2<=n) {
+            cout << num_2 << endl;
+        }
+        num_1=num_0;
+        num_0=num_2;
+    }
+
+    return 0;
 }
